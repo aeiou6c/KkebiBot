c3f225ae380dca14fee967cefc981a03c35bf976
KTY-Developer <kimty@pie.co.kr>
2021-04-23 18:18:42 +0900

    초기 커밋.

---
+++ HelpFormmatters/SimpleHelpFormatter.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Converters;
+using DSharpPlus.CommandsNext.Entities;
+
+namespace KkebiBot.HelpFormmatters
+{
+    public class SimpleHelpFormatter : BaseHelpFormatter
+    {
+        private StringBuilder MessageBuilder { get; }
+
+        public SimpleHelpFormatter(CommandContext ctx) : base(ctx)
+        {
+            this.MessageBuilder = new StringBuilder();
+        }
+
+        // this method is called first, it sets the command
+        public override BaseHelpFormatter WithCommand(Command command)
+        {
+            this.MessageBuilder.Append("Command: ")
+               .AppendLine(Formatter.Bold(command.Name))
+               .AppendLine();
+
+
+            this.MessageBuilder.Append("Description: ")
+                .AppendLine(command.Description)
+                .AppendLine();
+
+            if (command is CommandGroup)
+                this.MessageBuilder.AppendLine("This group has a standalone command.").AppendLine();
+
+            this.MessageBuilder.Append("Aliases: ")
+                .AppendLine(string.Join(", ", command.Aliases))
+                .AppendLine();
+
+
+            foreach (var overload in command.Overloads)
+            {
+                if (overload.Arguments.Count == 0)
+                {
+                    continue;
+                }
+
+                this.MessageBuilder.Append($"[Overload {overload.Priority}] Arguments: ")
+                .AppendLine(string.Join(", ", overload.Arguments.Select(xarg => $"{xarg.Name} ({xarg.Type.Name})")))
+                .AppendLine();
+            }
+
+            return this;
+        }
+
+        // this method is called second, it sets the current group's subcommands
+        // if no group is being processed or current command is not a group, it 
+        // won't be called
+        public override BaseHelpFormatter WithSubcommands(IEnumerable<Command> subcommands)
+        {
+            this.MessageBuilder.Append("Subcommands: ")
+                .AppendLine(string.Join(", ", subcommands.Select(xc => xc.Name)))
+                .AppendLine();
+
+            return this;
+        }
+
+        // this is called as the last method, this should produce the final 
+        // message, and return it
+        public override CommandHelpMessage Build()
+        {
+            return new CommandHelpMessage(this.MessageBuilder.ToString().Replace("\r\n", "\n"));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,203 @@
+﻿
+using KkebiBot.CommandModules;
+using KkebiBot.HelpFormmatters;
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.Entities;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using DSharpPlus.Interactivity.Extensions;
+using DSharpPlus.Interactivity.Enums;
+
+namespace KkebiBot
+{
+    public class Program
+    {
+        public readonly EventId BotEventId = new EventId(1, "Kkebi-Bot");
+
+        public DiscordClient Client { get; set; }
+        public InteractivityExtension Interactivity { get; set; }
+        public CommandsNextExtension Commands { get; set; }
+
+        public static void Main(string[] args)
+        {
+            // since we cannot make the entry method asynchronous,
+            // let's pass the execution to asynchronous code
+            var prog = new Program();
+            prog.RunBotAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task RunBotAsync()
+        {
+            // first, let's load our configuration file
+            var json = "";
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
+
+            // next, let's load the values from that file
+            // to our client's configuration
+            var cfgjson = JsonConvert.DeserializeObject<ConfigJson>(json);
+            var cfg = new DiscordConfiguration
+            {
+                Intents = DiscordIntents.All,
+                Token = cfgjson.Token,
+                TokenType = TokenType.Bot,
+
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug
+            };
+
+            // then we want to instantiate our client
+            this.Client = new DiscordClient(cfg);
+
+            // next, let's hook some events, so we know
+            // 이벤트 연결
+            // what's going on
+            this.Client.Ready += this.Client_Ready;
+            this.Client.GuildAvailable += this.Client_GuildAvailable;
+            this.Client.ClientErrored += this.Client_ClientError;
+
+            // let's enable interactivity, and set default options
+            this.Client.UseInteractivity(new InteractivityConfiguration
+            {
+                // default pagination behaviour to just ignore the reactions
+                PaginationBehaviour = PaginationBehaviour.Ignore,
+
+                // default timeout for other actions to 2 minutes
+                Timeout = TimeSpan.FromMinutes(2)
+            });
+
+            // up next, let's set up our commands
+            var ccfg = new CommandsNextConfiguration
+            {
+                // let's use the string prefix defined in config.json
+                // config.json 에 있는 prefix 사용
+                StringPrefixes = new[] { cfgjson.CommandPrefix },
+
+                // enable responding in direct messages
+                // 쪽지에서 응답 가능
+                EnableDms = true,
+
+                // enable mentioning the bot as a command prefix
+                // 봇을 command prefix로 언급하도록 설정
+                EnableMentionPrefix = true
+            };
+
+            // and hook them up
+            this.Commands = this.Client.UseCommandsNext(ccfg);
+
+            // let's hook some command events, so we know what's 
+            // going on
+            this.Commands.CommandExecuted += this.Commands_CommandExecuted;
+            this.Commands.CommandErrored += this.Commands_CommandErrored;
+
+            // up next, let's register our commands
+            this.Commands.RegisterCommands<ExampleUngrouppedCommands>();
+            this.Commands.RegisterCommands<ExampleGrouppedCommands>();
+            this.Commands.RegisterCommands<ExampleExecutableGroup>();
+
+            // up next, let's register our commands
+            this.Commands.RegisterCommands<ExampleInteractiveCommands>();
+
+            // set up our custom help formatter
+            this.Commands.SetHelpFormatter<SimpleHelpFormatter>();
+
+            // finally, let's connect and log in
+            await this.Client.ConnectAsync();
+
+            // and this is to prevent premature quitting
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Ready(DiscordClient sender, ReadyEventArgs e)
+        {
+            // let's log the fact that this event occured
+            sender.Logger.LogInformation(BotEventId, "Client is ready to process events.");
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private Task Client_GuildAvailable(DiscordClient sender, GuildCreateEventArgs e)
+        {
+            // let's log the name of the guild that was just
+            // sent to our client
+            sender.Logger.LogInformation(BotEventId, $"Guild available: {e.Guild.Name}");
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private Task Client_ClientError(DiscordClient sender, ClientErrorEventArgs e)
+        {
+            // let's log the details of the error that just 
+            // occured in our client
+            sender.Logger.LogError(BotEventId, e.Exception, "Exception occured");
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private Task Commands_CommandExecuted(CommandsNextExtension sender, CommandExecutionEventArgs e)
+        {
+            // let's log the name of the command and user
+            e.Context.Client.Logger.LogInformation(BotEventId, $"{e.Context.User.Username} successfully executed '{e.Command.QualifiedName}'");
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private async Task Commands_CommandErrored(CommandsNextExtension sender, CommandErrorEventArgs e)
+        {
+            // let's log the error details
+            e.Context.Client.Logger.LogError(BotEventId, $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}", DateTime.Now);
+
+            // let's check if the error is a result of lack
+            // of required permissions
+            if (e.Exception is ChecksFailedException ex)
+            {
+                // yes, the user lacks required permissions, 
+                // let them know
+
+                var emoji = DiscordEmoji.FromName(e.Context.Client, ":no_entry:");
+
+                // let's wrap the response into an embed
+                var embed = new DiscordEmbedBuilder
+                {
+                    Title = "Access denied",
+                    Description = $"{emoji} You do not have the permissions required to execute this command.",
+                    Color = new DiscordColor(0xFF0000) // red
+                };
+                await e.Context.RespondAsync(embed);
+            }
+        }
+    }
+
+    // this structure will hold data from config.json
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("prefix")]
+        public string CommandPrefix { get; private set; }
+    }
+}
